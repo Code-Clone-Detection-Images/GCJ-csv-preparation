bc39e14fe5c0fa10ea9a8dbca1e2d135cef34c36
Florian Sihler <florian.sihler@uni-ulm.de>
2022-02-12 12:13:23 +0100

    space management

--- prepare.py
+++ prepare.py
@@ -1,13 +1,15 @@
 from collections import defaultdict
 import csv
 import os
-from typing import TypedDict, List, Dict, DefaultDict, cast, Union
+from typing import TypedDict, List, Dict, DefaultDict, cast
+from itertools import product
 from enum import Enum
 from os import path
 
+
 class GcjFileSolution(Enum):
-    SMALL = 0
-    LARGE = 1
+    SMALL = '0'
+    LARGE = '1'
 
 
 class GcjFile(TypedDict):
@@ -25,16 +27,20 @@ Username = str
 
 class GcjMapping(TypedDict):
     name: str
-    java_files: DefaultDict[Username, List[GcjFile]]
-    c_files: DefaultDict[Username, List[GcjFile]]
+    java_small_files: DefaultDict[Username, List[GcjFile]]
+    java_large_files: DefaultDict[Username, List[GcjFile]]
+    c_small_files: DefaultDict[Username, List[GcjFile]]
+    c_large_files: DefaultDict[Username, List[GcjFile]]
 
 
 # task mapping 6254486
 TASK_MAPPING: Dict[str, GcjMapping] = {
     '5634697451274240': {
         'name': "Qualification Round -- Revenge of the Pancakes",
-        'java_files': defaultdict(lambda: []),
-        'c_files': defaultdict(lambda: [])
+        'java_small_files': defaultdict(lambda: []),
+        'java_large_files': defaultdict(lambda: []),
+        'c_small_files': defaultdict(lambda: []),
+        'c_large_files': defaultdict(lambda: [])
     }
 }
 
@@ -70,26 +76,22 @@ def __usable_c(file: GcjFile) -> bool:
 
 def assign_csv(files: List[GcjFile]):
     for file in files:
-        # file['solution'] == '0' TODO: separate solution
+        suffix = 'small' if file['solution'] == '0' else 'large'
         if __is_known_java(file) and __usable_java(file):
-            TASK_MAPPING[file['task']]['java_files'][file['username']].append(file)
+            TASK_MAPPING[file['task']
+                         ][f'java_{suffix}_files'][file['username']].append(file)
         elif __is_known_c(file) and __usable_c(file):
-            TASK_MAPPING[file['task']]['c_files'][file['username']].append(file)
+            TASK_MAPPING[file['task']
+                         ][f'c_{suffix}_files'][file['username']].append(file)
 
-
-def for_file(path_prefix: str, solution: int, files: List[GcjFile]) -> None:
-    for file in filter(lambda f: f['solution'] == str(solution), files):
-        target = os.path.join(
-            path_prefix, 'large' if file['solution'] == '1' else 'small')
-        os.makedirs(target, exist_ok=True)
-        with open(os.path.join(target, os.path.basename(file['full_path'])), 'w') as f:
-            f.write(file['flines'])
 
 def process_task_mapping() -> None:
     for key, value in TASK_MAPPING.items():
         print(f'  * {value["name"]}')
-        print(f'    - [java] {len(value["java_files"])} users')
-        print(f'    - [c]    {len(value["c_files"])} users')
+        print(
+            f'    - [java] {len(value["java_small_files"])} / {len(value["java_large_files"])} users  (small / large)')
+        print(
+            f'    - [c]    {len(value["c_small_files"])} / {len(value["c_large_files"])} users  (small / large')
         extract_task(value)
 
 
@@ -97,19 +99,34 @@ def process_task_mapping() -> None:
 def extract_task(value: GcjMapping) -> None:
     prefix = path.join("gcj", value["name"])
     os.makedirs(prefix, exist_ok=True, mode=0o777)
-    extract_file(prefix, value, "java")
-    extract_file(prefix, value, "c")
+    file_combs = [["java", "c"], [
+        GcjFileSolution.SMALL, GcjFileSolution.LARGE]]
+    for combs in product(*file_combs):
+        extract_file(prefix, value, combs[0], combs[1])
 
 
-def extract_file(prefix: str, value: GcjMapping, file_type: str):
+def extract_file(prefix: str, value: GcjMapping, file_type: str, solution: GcjFileSolution):
     assert file_type == "java" or file_type == "c"
-    prefix = path.join(prefix, file_type)
+    solution_string = "small" if solution == GcjFileSolution.SMALL else "large"
+    prefix = path.join(prefix, file_type + "-" + solution_string)
     os.makedirs(prefix, exist_ok=True, mode=0o777)
-    for user, files in value[f'{file_type}_files'].items():
+    for user, files in value[f'{file_type}_{solution_string}_files'].items():
         user_prefix = path.join(prefix, user)
         os.makedirs(user_prefix, exist_ok=True, mode=0o777)
-        for_file(user_prefix, 1, files)
-        for_file(user_prefix, 0, files)
+        for_file(user_prefix, 1, files, file_type == 'c')
+        for_file(user_prefix, 0, files, file_type == 'c')
+
+
+def for_file(path_prefix: str, solution: int, files: List[GcjFile], kill_space: bool = False) -> None:
+    for file in filter(lambda f: f['solution'] == str(solution), files):
+        target = os.path.join(
+            path_prefix, 'large' if file['solution'] == '1' else 'small')
+        os.makedirs(target, exist_ok=True)
+        filename = os.path.basename(file['full_path'])
+        if kill_space:  # spaces are a problem. some tools like cccd do not allow them
+            filename = filename.replace(' ', '-')
+        with open(os.path.join(target, ), 'w') as f:
+            f.write(file['flines'])
 
 
 if __name__ == '__main__':
